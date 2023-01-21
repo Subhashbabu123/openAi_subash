ef44b50b7b77f9dea6ee611ac2868aea53887080
Subhashbabu123 <110827222+Subhashbabu123@users.noreply.github.com>
2023-01-21 15:28:34 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://subhash.onrender.com', {
+    const response = await fetch('https://subhashbabu.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
